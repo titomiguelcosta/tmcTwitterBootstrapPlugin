84e715c5bcbc4fe74cbaf924924aa9e18263e893
Ben Lancaster <benlanc+github@ster.me.uk>
2012-06-11 05:56:13 -0700

    Making Bootstrap and JQuery paths portable.

--- modules/tmcTwitterBootstrap/templates/_assets.php
+++ modules/tmcTwitterBootstrap/templates/_assets.php
@@ -1,10 +1,10 @@
 <?php use_helper('Url'); ?>
 <!-- stylesheets -->
-<?php use_stylesheet(public_path('tmcTwitterBootstrapPlugin/css/bootstrap.min.css'), 'first') ?>
-<?php use_stylesheet(public_path('tmcTwitterBootstrapPlugin/css/bootstrap-responsive.min.css'), 'first') ?>
-<?php use_stylesheet(public_path('tmcTwitterBootstrapPlugin/css/styles.css'), 'first') ?>
+<?php use_stylesheet(sfConfig::get('app_tmcTwitterBootstrapPlugin_bootstrap_path',public_path('tmcTwitterBootstrapPlugin/css/bootstrap.min.css')), 'first') ?>
+<?php use_stylesheet(sfConfig::get('app_tmcTwitterBootstrapPlugin_responsive_bootstrap_path',public_path('tmcTwitterBootstrapPlugin/css/bootstrap-responsive.min.css')), 'first') ?>
+<?php use_stylesheet(sfConfig::get('app_tmcTwitterBootstrapPlugin_admin_styles_path',public_path('tmcTwitterBootstrapPlugin/css/styles.css')), 'first') ?>
 
 <!-- javascripts -->
-<?php use_javascript(public_path('tmcTwitterBootstrapPlugin/js/jquery.min.js')) ?>
-<?php use_javascript(public_path('tmcTwitterBootstrapPlugin/js/bootstrap.min.js')) ?>
-<?php use_javascript(public_path('tmcTwitterBootstrapPlugin/js/global.js')) ?>
+<?php use_javascript(sfConfig::get('app_tmcTwitterBootstrapPlugin_jquery_path',public_path('tmcTwitterBootstrapPlugin/js/jquery.min.js')),'first') ?>
+<?php use_javascript(sfConfig::get('app_tmcTwitterBootstrapPlugin_bootstrap_js_path',public_path('tmcTwitterBootstrapPlugin/js/bootstrap.min.js'))) ?>
+<?php use_javascript(sfConfig::get('app_tmcTwitterBootstrapPlugin_admin_js_path',public_path('tmcTwitterBootstrapPlugin/js/global.js')), 'last') ?>
